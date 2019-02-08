424d5240a9007afc61efa1f838a806d77f5203f2
Guactoshi <cmilohernandez@gmail.com>
2019-02-08 00:30:58 -0500

    Initital commit. Added Vue template and Crud firebase application.

---
+++ precache-manifest.a90244f12a5067f7b0f17d54be072e63.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fec7ab17ab62fdb85076",
+    "url": "/js/app.bce65714.js"
+  },
+  {
+    "revision": "de29c02ec4d000b6bdff",
+    "url": "/js/chunk-vendors.75083d90.js"
+  },
+  {
+    "revision": "c6fdeffa05626a7829c4219e4ea6038b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
